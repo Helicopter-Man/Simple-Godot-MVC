a07706c738aa4d2011fcb2df4bc4dc7c7b275256
HM <1728741310@qq.com>
2026-03-19 21:21:43 +0800

    代码调整

--- framework/kits/resource_kit.gd
+++ framework/kits/resource_kit.gd
@@ -2,6 +2,7 @@ extends Resource
 ## 一个简单的存储特定的Resource到特定位置的工具
 class_name ResourceKit
 
+## 保存资源
 static func resource_save(resource : Resource,save_path : String) -> void:
 	# 确保路径使用 user:// 目录
 	if not save_path.begins_with("user://"):
@@ -12,18 +13,12 @@ static func resource_save(resource : Resource,save_path : String) -> void:
 		push_error("ResourceKit|资源保存|路径非法，确保文件后缀使用 .tres")
 		return
 
-	# 确保路径存在
-	var dir = DirAccess.open("user://")
-	var dir_path = save_path.get_base_dir().replace("user://", "")
-	for part in dir_path.split("/"):
-		if part != "":
-			if not dir.dir_exists(part):
-				if dir.make_dir(part) != OK:
-					return
-			dir = DirAccess.open(dir.get_current_dir().path_join(part))
+	# 确保文件夹存在
+	_ensure_folder_exists(save_path)
 
 	ResourceSaver.save(resource,save_path)
 
+## 读取资源
 static func resource_load(save_path : String) -> Resource:
 	# 确保路径使用 user:// 目录
 	if not save_path.begins_with("user://"):
@@ -37,7 +32,10 @@ static func resource_load(save_path : String) -> Resource:
 	# 检查文件是否存在
 	if not FileAccess.file_exists(save_path):
 		push_warning("ResourceKit|资源读取|文件不存在" + save_path)
+		# 生成相应文件夹
+		_ensure_folder_exists(save_path)
 		return null
+
 
 	# NOTE 深拷贝，防止直接使用资源文件
 	var resource: Resource = load(save_path)
@@ -46,3 +44,14 @@ static func resource_load(save_path : String) -> Resource:
 		resource = resource.duplicate(true)
 
 	return resource
+
+## 确保路径存在
+static func _ensure_folder_exists(save_path : String) -> void:
+	var dir = DirAccess.open("user://")
+	var dir_path = save_path.get_base_dir().replace("user://", "")
+	for part in dir_path.split("/"):
+		if part != "":
+			if not dir.dir_exists(part):
+				if dir.make_dir(part) != OK:
+					return
+			dir = DirAccess.open(dir.get_current_dir().path_join(part))
